df53b2c7d76c3b6d7eb49d883b5477ed1a491666
rachel <rachelohler@gmail.com>
2020-09-14 11:03:53 -0400

    created a get method for database

--- src/app/inspirit.service.ts
+++ src/app/inspirit.service.ts
@@ -2,10 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InspiritService {
   apiUrl: string = 'https://api.unsplash.com/search/photos';
   clientID: string = 'iKevvcDauVZXBrlQ1HZg_DKN-naujnI7zD_5ZcVu-JU';
-  constructor(private http: HttpClient) { }
+  // unsplash^^^
+  databaseUrl: string = 'http://localhost:3000';
+  constructor(private http: HttpClient) {}
+
+  // getUnsplash = (searchTerm: string) => {
+  //   return this.http.get(this.apiUrl, {
+  //     params: {
+  //       client_id: this.clientID,
+  //       query: searchTerm,
+  //       page:
+  //     },
+  //   });
+  // };
+  // help ^^^
+
+  getQuotes = (): any => {
+    return this.http.get(`${this.databaseUrl}/quotes`);
+  };
 }
