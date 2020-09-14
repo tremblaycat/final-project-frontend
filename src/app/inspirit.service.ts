a192c5a76f5f6fe78ade7207c40de7c64d97e4ed
demetrinh <demetrinh@gmail.com>
2020-09-14 15:04:58 -0400

    updated service and results

--- src/app/inspirit.service.ts
+++ src/app/inspirit.service.ts
@@ -5,22 +5,24 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class InspiritService {
-  apiUrl: string = 'https://api.unsplash.com/search/photos';
-  clientID: string = 'iKevvcDauVZXBrlQ1HZg_DKN-naujnI7zD_5ZcVu-JU';
+  apiUrl: string = 'https://api.unsplash.com/photos/random';
+  clientID: string = 'y_3z3WhDc7C_SPHhZoOuYWwWEPV-zAtJb9P6IxLXE8Q';
   // unsplash^^^
   databaseUrl: string = 'http://localhost:3000';
   constructor(private http: HttpClient) {}
 
-  // getUnsplash = (searchTerm: string) => {
-  //   return this.http.get(this.apiUrl, {
-  //     params: {
-  //       client_id: this.clientID,
-  //       query: searchTerm,
-  //       page:
-  //     },
-  //   });
-  // };
-  // help ^^^
+  getUnsplash = () => {
+    return this.http.get(this.apiUrl, {
+      params: {
+        client_id: this.clientID,
+        collections: '827743',
+      },
+    });
+  };
+
+  getPhoto() {
+    return this.http.get(`${this.apiUrl}`);
+  }
 
   getQuotes = (): any => {
     return this.http.get(`${this.databaseUrl}/quotes`);
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InspiritService } from '../inspirit.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
 })
 export class ResultsComponent implements OnInit {
-isCollapsed: boolean = true;
-  constructor() { }
+  isCollapsed: boolean = true;
+  randomPhoto: any;
+  constructor(
+    private route: ActivatedRoute,
+    private service: InspiritService
+  ) {}
 
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
   }
   ngOnInit(): void {
+    this.getPhoto();
   }
 
+  getPhoto = (): any => {
+    this.service.getUnsplash().subscribe((response) => {
+      console.log(response);
+      this.randomPhoto = response;
+    });
+  };
 }
